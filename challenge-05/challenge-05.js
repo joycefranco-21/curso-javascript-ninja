bf01ff7b62572956957bbb5f83023c40e1932e33
Joyce Franco <jooyce21@gmail.com>
2026-02-09 11:24:08 -0300

    first exercises

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,22 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// 
+let sampleValues = [ 2, 'test', true, 2.5, 'joyce' ]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+// 
+function sampleFunction(arr) {
+    return arr
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+// 
+console.log(sampleFunction(sampleValues)[1])
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,7 +25,8 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+// 
+
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
